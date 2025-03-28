3dc33176ae4d99bdeb3f1d3681c6a9e748ab9480
rzlzrdf <rizallazu@student.ub.ac.id>
2025-03-28 20:13:57 +0700

    add user type

--- schemaTypes/userType.ts
+++ schemaTypes/userType.ts
@@ -1,17 +1,61 @@
 import {defineField, defineType} from 'sanity'
 
-
 export const userType = defineType({
-    name: "user",
-    title: "User",
-    type: "document",
-    fields: [
-        defineField({
-            name: "name",
-            title: "Name",
-            type: "string",
-            validation: (Rule) => Rule.required(),
-            description: "Name of the user",
-        }),
-    ]
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  preview: {
+    select: {
+      title: 'name',
+    },
+    prepare({title}) {
+      return {
+        title: title || 'No name',
+      }
+    },
+  },
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+      description: 'Name of the user',
+    }),
+    defineField({
+      name: 'nisn',
+      title: 'NISN or NIP',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+      description: 'NISN of the student or NIP for Teacher',
+    }),
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: (Rule) => Rule.required().email(),
+      description: 'Email of the user',
+    }),
+    defineField({
+      name: 'password',
+      title: 'Password',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+      description: 'Password of the user',
+    }),
+    defineField({
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Student', value: 'student'},
+          {title: 'Teacher', value: 'teacher'},
+          {title: 'Admin', value: 'admin'},
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+      description: 'Role of the user',
+    }),
+  ],
 })
